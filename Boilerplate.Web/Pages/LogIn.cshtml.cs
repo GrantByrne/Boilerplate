39d4924a9d2213b463dfdcf42ac989058af5c1ac
Grant Byrne <grant@grantbyrne.com>
2024-11-07 23:12:29 -0600

    Implemented the Login page

--- Boilerplate.Web/Pages/LogIn.cshtml.cs
+++ Boilerplate.Web/Pages/LogIn.cshtml.cs
@@ -1,11 +1,51 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Boilerplate.Web.Pages;
 
-public class LogIn : PageModel
+public class LoginModel(SignInManager<IdentityUser> signInManager) : PageModel
 {
-    public void OnGet()
+    [BindProperty]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email address.")]
+    public string Email { get; set; }
+
+    [BindProperty]
+    [Required(ErrorMessage = "Password is required.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [BindProperty]
+    public bool RememberMe { get; set; }
+
+    public string ReturnUrl { get; set; }
+
+    public void OnGet(string returnUrl = null)
     {
-        
+        ReturnUrl = returnUrl ?? Url.Content("~/");
+    }
+
+    public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+    {
+        returnUrl ??= Url.Content("~/");
+
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var result = await signInManager.PasswordSignInAsync(Email, Password, RememberMe, lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            return LocalRedirect(returnUrl);
+        }
+        else
+        {
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return Page();
+        }
     }
 }
